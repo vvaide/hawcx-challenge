3f9823956b3f7f688595f99feeafde5715ba72ad
vedant vaideswar <vedantvaideswar@gmail.com>
2025-03-02 00:41:12 -0600

    Fixed logging

--- frontend.py
+++ frontend.py
@@ -68,29 +68,29 @@ class Login(Container):
 
     async def get_challenge(self, email: str) -> str:
         async with httpx.AsyncClient(verify=False) as client:
-            logger.warning(f"VEDU - Sending challenge request to {BACKEND_URL}/challenge")
+            logger.warning(f"log >>> - Sending challenge request to {BACKEND_URL}/challenge")
             response = await client.post(f"{BACKEND_URL}/challenge", json={"email": email})
-            logger.warning(f"VEDU - Response: {response.json()}")
+            logger.warning(f"log >>> - Response: {response.json()}")
             return response.json()["challenge"]
 
     async def send_registration(self, email: str, credential: str) -> None:
-        logger.warning(f"VEDU - Sending registration request to {BACKEND_URL}/register")
+        logger.warning(f"log >>> - Sending registration request to {BACKEND_URL}/register")
         async with httpx.AsyncClient(verify=False) as client:
             response = await client.post(
                 f"{BACKEND_URL}/register", 
                 json={"email": email, "credential": credential}
             )
-            logger.warning(f"VEDU - Response: {response.json()}")
+            logger.warning(f"log >>> - Response: {response.json()}")
             return response.json() 
 
     async def register_user(self, event: ControlEvent) -> None:
-        logger.warning("VEDU - Registering user")
+        logger.warning("log >>> - Registering user")
         if not self.email.value:
-            logger.warning("VEDU - No email provided")
+            logger.warning("log >>> - No email provided")
             self.status_text.value = "Please enter your email"
             self.status_text.color = Colors.RED
             self.update()
-            logger.warning("VEDU - Exiting register_user")
+            logger.warning("log >>> - Exiting register_user")
             return
         
         self.status_text.value = "Starting biometric registration..."
@@ -99,7 +99,7 @@ class Login(Container):
         
         # get challenge from server
         challenge = await self.get_challenge(self.email.value)
-        logger.warning(f"VEDU - Challenge: {challenge}")
+        logger.warning(f"log >>> - Challenge: {challenge}")
 
         # navigator.credentials.create
         pub_key = PublicKeyCredentialCreationOptions(
@@ -137,23 +137,23 @@ class Login(Container):
         
         # convert to json
         pub_key_json = options_to_json(pub_key)
-        logger.warning(f"VEDU - Options: {pub_key_json}")
+        logger.warning(f"log >>> - Options: {pub_key_json}")
         
         # send options to server
         res = await self.send_registration(self.email.value, pub_key_json)
-        logger.warning(f"VEDU - Response: {res}")
+        logger.warning(f"log >>> - Response: {res}")
         res = json.loads(res)
         if not res["success"]:
             self.status_text.value = "Registration failed"
             self.status_text.color = Colors.RED
             self.update()
-            logger.warning("VEDU - Exiting register_user")
+            logger.warning("log >>> - Exiting register_user")
             return
 
         self.status_text.value = "Registration successful"
         self.status_text.color = Colors.GREEN
         self.update()
-        logger.warning("VEDU - Exiting register_user")
+        logger.warning("log >>> - Exiting register_user")
 
     def authenticate_user(self, event: ControlEvent) -> None:
         if not self.email.value:
@@ -166,15 +166,15 @@ class Login(Container):
         self.update()
         
 def main(page: Page) -> None:
-    logger.warning("VEDU - Starting app")
+    logger.warning("log >>> - Starting app")
     page.title = "Hawcx Challenge"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.theme_mode = ft.ThemeMode.DARK
-    logger.warning("VEDU - Adding login page")
+    logger.warning("log >>> - Adding login page")
     page.add(Login())
-    logger.warning("VEDU - Exiting main")
+    logger.warning("log >>> - Exiting main")
 
 if __name__ == "__main__":
-    logger.warning("VEDU - Starting app")
+    logger.warning("log >>> - Starting app")
     app(target=main, view=ft.AppView.WEB_BROWSER)
